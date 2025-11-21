1df2428cb4a0a0c08cc5262feec87372906e5291
aishwaryaks167 <aishwaryaks167@gmail.com>
2025-11-21 15:45:04 +0530

    Add files via upload

---
+++ aishwarya.k s/calls_reference.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void sum_value(int a,int b)
+{
+    int sum = a+b;
+    printf("%d\n", sum);
+}
+void sum_ref(int *a, int *b)
+{
+    int sum = *a+*b;
+    printf("%d\n", sum);
+}
+void main()
+{
+    int a = 99;
+    int b = 18;
+    printf("call by value: \n");
+    sum_value(a,b);
+    printf("call by reference: \n");
+    sum_ref(&a,&b);
+}
